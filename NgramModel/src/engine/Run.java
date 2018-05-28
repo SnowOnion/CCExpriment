6cc04bfcfb11338034e3abe0ea04a3a8649c761c
SunshineAllWay <mhnfwln@126.com>
2018-05-28 18:00:48 +0800

    [Keep Moving 10] [28, May, 2018] Succussful run and correct output. Structure of code need to be polished. Class and functions relevant to Type var K need to be redesigned.

--- NgramModel/src/engine/Run.java
+++ NgramModel/src/engine/Run.java
@@ -1,14 +1,38 @@
 package engine;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
 
-import model.BasicNGram;
+import model.BasicNCharGram;
+import unit.Tokensequence;
 
 
 public class Run implements AppRunNGram{
 	public void run() {
-		BasicNGram<Integer> bn = new BasicNGram<>(4);
-		bn.preAction(new File("C:\\Users\\HHeart\\Desktop\\CodeCompletion\\CCExpriment\\Data\\dataset1\\8_37216915_2006-8-21_4.5.txt"));
-		System.out.println("OK");
+		BasicNCharGram bn = new BasicNCharGram(4);
+		File file = new File("C:\\Users\\HHeart\\Desktop\\CodeCompletion\\CCExpriment\\Data\\dataset1\\8_37216915_2006-8-21_4.5.txt");
+		bn.preAction(file);
+		
+		System.out.println("----------Model------------");
+		
+		System.out.print("N:");
+		System.out.println(bn.n);
+		
+		System.out.print("Size of ngramap:");
+		System.out.println(bn.getBasicNGramModel().keySet().size());
+		
+		ArrayList<Character> query = new ArrayList<Character>();
+		query.add('Îå');
+		query.add('ÐÇ');
+		query.add('¼¶');
+		query.add('±ö');
+				
+		Tokensequence<Character> queryseq = new Tokensequence<Character>(query);
+		Optional<Character> inferedWord = bn.tokenInference(queryseq); 
+		if (inferedWord.isPresent()) {
+			System.out.println(inferedWord.get());
+		}
 		return;
 	}
 }
--- NgramModel/src/unit/Tokenstream.java
+++ NgramModel/src/unit/Tokenstream.java
@@ -2,8 +2,11 @@ package unit;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Tokenstream<K> {
 	public File tokenSourceFile;  //sourcefile                    
@@ -16,23 +19,52 @@ public class Tokenstream<K> {
 		this.n = N;
 		this.tokenSourceFile = ptokenSourceFile;
 		
-		importStreamFromFile();
+		importTokentStreamFromFile();
 		convertWholeStreamToStreamList();
 	}
 	
+	
 	//import token stream from sourcefile
-	//TODO
-	private void importStreamFromFile() {
-		wholeStream = new ArrayList<K>();
+	private void importTokentStreamFromFile() {
+		ArrayList<Character> strStream = new ArrayList<Character>();
 		try {
-			FileInputStream inputstream = new FileInputStream(tokenSourceFile);
-			
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			//strStream = importCharStreamFromFile();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
+		convertCharStreamToTokenStream(strStream);
 	}
+	
+	
+	//import character stream from sourcefile
+	private ArrayList<K> importCharStreamFromFile() {
+        Reader reader = null;
+        ArrayList<K> strStream = new ArrayList<K>();
+        
+        try {
+            reader = new InputStreamReader(new FileInputStream(tokenSourceFile));
+            int tempchar;
+            while ((tempchar = reader.read()) != -1) {
+                if (((char) tempchar) != '\r') {
+                  //  wholeStream.strStream(new Character((char) tempchar));
+                }
+            }
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return strStream;
+    }
+
+	
+	//TO BE DISCUSSED and REFINED
+	private void convertCharStreamToTokenStream(ArrayList<Character> strStream) {
+		//wholeStream = new ArrayList<K>();
+		wholeStream = (ArrayList<K>) Arrays.asList(strStream.toArray());
+	}
+	
 	
 	//extract the streamList from token stream
 	private void convertWholeStreamToStreamList() {
@@ -48,8 +80,9 @@ public class Tokenstream<K> {
 			Tokensequence<K> tokenseq = new Tokensequence<K>(seq); 
 			streamList.add(tokenseq);
 		}
-	}
-	
+	}	
+
+
 	//get streamList
 	public ArrayList<Tokensequence<K>> getStreamList() {
 		return (this.streamList);
